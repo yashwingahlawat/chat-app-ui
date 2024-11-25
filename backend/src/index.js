d41bc010b4798c1d99091877f6ae2d81a079a328
yashwingahlawat <110126076+yashwingahlawat@users.noreply.github.com>
2024-11-26 03:21:02 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://chat-app-ui-omega-three.vercel.app/",
+    origin: "https://chat-app-ui-omega-three.vercel.app",
     credentials: true,
   })
 );
